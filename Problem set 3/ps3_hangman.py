8ef7a3f12bda165c04342aa338d7030147e45a49
Stanislav <sbelousov1985@gmail.com>
2019-07-29 21:09:13 +0500

    added problem set 3 (solution)

--- Problem set 3/ps3_hangman.py
+++ Problem set 3/ps3_hangman.py
@@ -50,7 +50,7 @@ def isWordGuessed(secretWord, lettersGuessed):
     returns: boolean, True if all the letters of secretWord are in lettersGuessed;
       False otherwise
     '''
-    # FILL IN YOUR CODE HERE...
+    return set(secretWord).issubset(set(lettersGuessed))
 
 
 
@@ -61,7 +61,10 @@ def getGuessedWord(secretWord, lettersGuessed):
     returns: string, comprised of letters and underscores that represents
       what letters in secretWord have been guessed so far.
     '''
-    # FILL IN YOUR CODE HERE...
+    result = secretWord
+    for x in list(set(secretWord).difference(set(lettersGuessed))):
+        result = result.replace(x, '_ ')
+    return result
 
 
 
@@ -71,7 +74,10 @@ def getAvailableLetters(lettersGuessed):
     returns: string, comprised of letters that represents what letters have not
       yet been guessed.
     '''
-    # FILL IN YOUR CODE HERE...
+    result = 'abcdefghijklmnopqrstuvwxyz'
+    for letter in lettersGuessed:
+        result = result.replace(letter, '')
+    return result
     
 
 def hangman(secretWord):
@@ -94,8 +100,36 @@ def hangman(secretWord):
 
     Follows the other limitations detailed in the problem write-up.
     '''
-    # FILL IN YOUR CODE HERE...
-
+    mistakesMade = 0
+    lettersGuessed = []
+    print("Welcome to the game, Hangman!")
+    template = "I am thinking of a word that is {} letters long."
+    print(template.format(len(secretWord)))
+    print('-------------')
+    while 8 - mistakesMade > 0:
+        availableLetters = getAvailableLetters(lettersGuessed)
+        template = "You have {} guesses left.\
+                    \nAvailable letters: {}\
+                    \nPlease guess a letter: "
+        guess = input(template.format(8 - mistakesMade, availableLetters))
+        guessInLowerCase = guess.lower()
+        if guessInLowerCase in lettersGuessed:
+            template = "Oops! You've already guessed that letter: {}"
+        else:
+            lettersGuessed.append(guessInLowerCase)
+            if guessInLowerCase in secretWord:
+                template = "Good guess: {}"
+            else:        
+                template = "Oops! That letter is not in my word: {}"
+                mistakesMade += 1
+        print(template.format(getGuessedWord(secretWord, lettersGuessed)))
+        print('-------------')
+        if isWordGuessed(secretWord, lettersGuessed):
+            print('Congratulations, you won!')
+            break
+    if mistakesMade == 8:
+        template = 'Sorry, you ran out of guesses. The word was {}.'
+        print(template.format(secretWord))
 
 
 
@@ -105,5 +139,5 @@ def hangman(secretWord):
 # and run this file to test! (hint: you might want to pick your own
 # secretWord while you're testing)
 
-# secretWord = chooseWord(wordlist).lower()
-# hangman(secretWord)
+secretWord = chooseWord(wordlist).lower()
+hangman(secretWord)
